373d066339b255d6f510b306a9c55462d4bd840e
Chandra Prakash Jangid <talkme.kumar@gmail.com>
2022-07-11 17:16:26 +0530

    Save model created

--- ai.py
+++ ai.py
@@ -83,6 +83,17 @@ class Dqn():
         if len(self.reward_window)>1000:
             del self.reward_window[0]
         return action
+    
+    
+    def score(self):
+        return sum(self.reward_window)/(len(self.reward_window)+1)
+    
+    
+    def save(self):
+        torch.save({'state_dict':self.model.state_dict(),
+                    'optimizer':self.optimizer.state_dict,             
+                    }, 'last_brain.pth')
+    
         
         
     
